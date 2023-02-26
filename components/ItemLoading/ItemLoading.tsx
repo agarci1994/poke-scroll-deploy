f1f92d5c516bc46bc0220ac897cb53f86f1e657c
Alejandro García <alejandro.garci.rodr@gmail.com>
2023-02-26 09:44:05 +0100

    change color place holder

--- components/ItemLoading/ItemLoading.tsx
+++ components/ItemLoading/ItemLoading.tsx
@@ -8,9 +8,9 @@ export const ItemLoading = forwardRef<Ref, any>(function ItemLoading(_, ref) {
       className="border rounded-lg px-4 py-6"
     >
       <div className="flex flex-col space-y-4 h-full items-center justify-center text-center">
-        <div className="animate-pulse bg-primary rounded-lg w-32 h-32 max-w-full" />
-        <div className="animate-pulse bg-yellow rounded-lg w-12 h-8" />
-        <div className="animate-pulse bg-black rounded-lg w-48 max-w-full h-12" />
+        <div className="animate-pulse bg-secondary rounded-lg w-32 h-32 max-w-full" />
+        <div className="animate-pulse bg-black rounded-lg w-12 h-7" />
+        <div className="animate-pulse bg-primary rounded-lg w-48 max-w-full h-12" />
       </div>
     </div>
   )
