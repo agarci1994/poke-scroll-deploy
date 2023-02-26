06557a82388bc38ff0ab41d8a856c94a2c5ff60e
Alejandro García <alejandro.garci.rodr@gmail.com>
2023-02-26 17:15:25 +0100

    eslint ignore jest file

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,12 +1,16 @@
-import Pokemon from "../components/Pokemon/Pokemon"
+/* eslint-disable no-undef */
+import Pokemon from '../components/Pokemon/Pokemon'
 
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 
 describe('Pokemon', () => {
   it('renders a Pokemon', () => {
-    const { image, name, types, id } = {
-      image: "url", name: "pikachu", types: [], id: 1
+    const { name } = {
+      image: 'url',
+      name: 'pikachu',
+      types: [],
+      id: 1,
     }
     render(<Pokemon types={[]} name={name} />)
     expect(screen.getByText(name)).toBeTruthy()
